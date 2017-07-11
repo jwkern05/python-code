e5f6c5acd29a5426b1efbb0208e003553b673112
Jeffrey Kern <jwkern05@gmail.com>
2017-07-10 20:31:51 -0400

    Print with price

--- groceries.py
+++ groceries.py
@@ -32,7 +32,8 @@ print("THERE ARE " + str(len(products)) + " PRODUCTS")
 products = sorted(products, key=operator.itemgetter("name"))
 
 for product in products:
-    print(" + " + product["name"])
+    price_usd = ' ${0:.2f}'.format(product["price"]) #formatting to dollars
+    print(" + " + product["name"] + price_usd)
 
 
 # code.interact(local=locals())
